8d83276b27358a1a7528ae456a00a37b62b314e3
Abdi Berhe <abdi201380@gmail.com>
2023-03-22 05:47:18 -0700

    program for removing top element

---
+++ pop.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * _pop - removes element from top of stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void _pop(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		_freeStack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	*head = h->next;
+	free(h);
+}
